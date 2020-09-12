4d55437fc8b7cd0e24b08525948f7fb310069681
Anatoly <kosov4235@yandex.ru>
2020-09-12 09:31:57 +0300

    Домашнее задание

--- src/lesson7/Cat.java
+++ src/lesson7/Cat.java
@@ -1,23 +1,45 @@
 package lesson7;
 
+
 public class Cat {
     private String name;
+    private int amount;
+    private int countEat;
 
-    public Cat(String name) {
-        this.name = name;
+
+    public Cat(int countEat) {
+        this.countEat = countEat;
     }
+
+
+    public Cat(String name, int amount, int countEat) {
+        this.name = name;
+        this.amount = amount;
+        this.countEat = countEat;
+
+    }
+
+    public void hungryCat() {
+        if (countEat < amount) {
+            System.out.println("Кот " + name + " голоден");
+        } else {
+            System.out.println("Кот " + name + " сыт");
+        }
+    }
+
 
     public void eat(Plate plate) {
-        System.out.println("Cat " + name + " eat...");
-        plate.decreaseFood(10);
+        System.out.println("Кот " + name + " ест...");
+
+        plate.decreaseFood(amount);
+
+
+        countEat = countEat + amount;
+        System.out.println(countEat);
+
     }
 
 
-    public String getName() {
-        return name;
-    }
-
-    public void fear(Dog dog){
-        System.out.println("Cat "+ name+" fear dog "+ dog.getName());
-    }
 }
+
+
